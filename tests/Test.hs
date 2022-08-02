d0efa9c7b5f88bb641e2f07119241f4b7dbb06da
Walker <walker@praiseweb.com.br>
2022-08-02 17:17:16 -0300

    test: add missing find

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,24 +1,26 @@
 module Test where
 
+import Action
+import Control.Monad (void)
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State.Strict
 import Test.Tasty
 import Test.Tasty.HUnit
-import Control.Monad (void)
-import Control.Monad.Trans.State.Strict
-import Control.Monad.Trans.Class
-import Action
 
 mkReadProcess :: Command -> String -> State [Command] String
 mkReadProcess c r = do
-		hs <- get
-		void $ put $ c:hs
-		return r
+  hs <- get
+  void $ put $ c : hs
+  return r
 
 instance EffectMonad (State [Command]) where
-	readProcess ("find", a) = mkReadProcess ("find", a) "bar\nfoo"
-	readProcess c = mkReadProcess c mempty
+  readProcess ("find", a) = mkReadProcess ("find", a) "bar\nfoo"
+  readProcess c = mkReadProcess c mempty
 
 main = defaultMain $
-	testCase "should run `nix-env -u` for each nix user" $ do
-		execState upgradeNix mempty @?=
-			[ ("su", ["-", "foo", "-c", "\"nix-env -u\""])
-		 	, ("su", ["-", "bar", "-c", "\"nix-env -u\""]) ]
+  testCase "should run `nix-env -u` for each nix user" $ do
+    execState upgradeNix mempty
+      @?= [ ("su", ["-", "foo", "-c", "\"nix-env -u\""]),
+            ("su", ["-", "bar", "-c", "\"nix-env -u\""]),
+            ("find", ["/nix/var/nix/profiles/per-user", "-type", "d", "-exec", "basename", "{}", "\\;"])
+          ]
