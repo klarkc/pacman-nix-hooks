5fed901ffa886a1beaf04ad54d2afbdddf5be687
Walker <walker@praiseweb.com.br>
2022-08-02 17:45:21 -0300

    feat: add upgrade-nix to upgradeNix

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import Action
+import System.Process as S
+
+instance EffectMonad IO where
+  readProcess (c, a) = S.readProcess c a mempty
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = upgradeNix
